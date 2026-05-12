23b3a45059e945c27cae64a61cb76cf14714d8a0
miyakoshi <kanata11510@outlook.jp>
2026-05-12 22:17:43 +0900

    コードパイプラインテスト

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from ECS Fargate!");
+app.MapGet("/", () => "Hello from ECS Fargate! パイプラインでデプロイテスト中");
 app.MapGet("/health", () => Results.Ok("healthy"));
 
 app.Run();
