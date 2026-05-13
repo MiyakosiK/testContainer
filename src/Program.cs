a801e620f65d07b26836661af7e4a818bcc59bf1
k.miyakoshi <k.miyakoshi@NAMIKAZE>
2026-05-13 14:13:40 +0900

    宮越リポジトリ最終デプロイテスト

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from ECS Fargate! パイプラインでデプロイテスト中");
+app.MapGet("/", () => "Hello from ECS Fargate! おはよう！14：00だよ！");
 app.MapGet("/health", () => Results.Ok("healthy"));
 
 app.Run();
